ac24406abca51a9f223f8fabf8695b35113f6088
utkarsh032 <utkarshhraj525@gmail.com>
2023-11-02 12:25:43 +0530

    router setup foq Quiz-App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Main from './components/Main';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Main></Main> },
+  { path: '/quiz', element: <Quiz></Quiz> },
+  { path: '/result', element: <Result></Result> }
+])
 
 function App() {
   return (
-    <div className="App">
-      React App
-    </div>
-  );
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App;
