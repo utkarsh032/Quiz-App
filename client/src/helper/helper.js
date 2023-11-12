31905967aa7a12e66c076b73727dd74d37092025
utkarsh032 <utkarshhraj525@gmail.com>
2023-11-12 17:22:38 +0530

    Cunsuming Api

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-
+import axios from 'axios'
 
 export function attempts_Number(result) {
   return result.filter(r => r !== undefined).length;
@@ -18,4 +18,17 @@ export function flagResult(totalPoints, earnPoints) {
 export function CheckUserExist({ children }) {
   const auth = useSelector(state => state.result.userId)
   return auth ? children : <Navigate to={'/'} replace={true}></Navigate>
+}
+
+/** get server data */
+export async function getServerData(url, callback) {
+  const data = await (await axios.get(url))?.data;
+  return callback ? callback(data) : data;
+}
+
+
+/** post server data */
+export async function postServerData(url, result, callback) {
+  const data = await (await axios.post(url, result))?.data;
+  return callback ? callback(data) : data;
 }
