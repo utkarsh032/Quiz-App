b311af081bfff42a49f5aa82ee56f031cfa55808
utkarsh032 <utkarshraj525@gmail.com>
2024-05-03 17:17:49 +0530

    err Fixed

--- server/models/resultSchema.js
+++ server/models/resultSchema.js
@@ -8,11 +8,11 @@ const resultSchema = new Schema({
   result: { type: Array, default: [] },
   attempts: { type: Number, default: 0 },
   points: { type: Number, default: 0 },
-  achieved: { type: String, default: '' }, // Correct the field name to "achieved"
-  createdAt: { type: Date, default: Date.now }
+  achieved: { type: String, default: "" }, // Correct the field name to "achieved"
+  createdAt: { type: Date, default: Date.now },
 });
 
 // Create and export the "Result" model
-const Result = mongoose.model('Result', resultSchema);
+const Result = mongoose.model("Result", resultSchema);
 
 export default Result;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import { config } from 'dotenv';
-import router from './router/route.js';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
+import path from "path"; // Import path module
+import router from "./router/route.js";
 
 /** import connection file */
-import connect from './database/connection.js';
+import connect from "./database/connection.js";
 
 const app = express();
 const _dirname = path.resolve();
 
-
 /** app middlewares */
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 config();
@@ -20,22 +20,21 @@ config();
 /** application port */
 const port = process.env.PORT || 8080;
 
-
 /** routes */
-app.use('/api', router); /** APIs */
+app.use("/api", router); /** APIs */
 
 app.use(express.static(path.join(_dirname, "client/build")));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   try {
     res.json("Get Request");
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(_dirname, "client/build", "index.html"));
 });
 
